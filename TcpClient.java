bf0d868e01f83db30a5298b68d98dbbd5ba15476
icju <isicju@gmail.com>
2024-01-02 00:45:14 +0000

    tcp client metric

--- TcpClient.java
+++ TcpClient.java
@@ -9,11 +9,12 @@ public class TcpClient {
         String fileLocation = args[0];
         try (Socket socket = new Socket("localhost", 7777)) {
             Path filePath = Paths.get(fileLocation);
-
+            long before = System.currentTimeMillis();
+            System.out.println("Starting copy process..");
             try (OutputStream outputStream = socket.getOutputStream()) {
                 java.nio.file.Files.copy(filePath, outputStream);
             }
-
+            System.out.println("total time " + (System.currentTimeMillis() - before)/1000f + " seconds");
             System.out.println("File sent successfully.");
         }
     }
